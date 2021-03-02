0b8aabca5264ab89a4a57780cab77d1a6ec71f0d
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2021-03-02 01:09:28 -0300

    ajustes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,28 @@
 class CartsController < ApplicationController
     def index
-        
-        @cart = Cart.by_user(current_user)
-        cp = CartProduct.where(cart_id: @cart.id)
-        
-        @products = []
-        cp.each{ |cp| @products << Product.find(cp.product_id) }
+        if current_user.nil?
+            redirect_to root_path, notice: "É necessário fazer login..."
+        else
 
+            @cart = Cart.by_user(current_user)
+            cp = CartProduct.where(cart_id: @cart.id)
+            
+            @products = []
+            cp.each{ |cp| @products << Product.find(cp.product_id) }
+        end
     end
+
+
+    def finish
+        if current_user.nil?
+            redirect_to root_path, notice: "É necessário fazer login..."
+        else
+            @cart = Cart.by_user(current_user)
+            CartProduct.where(cart_id: @cart.id).destroy_all
+        end
+
+        redirect_to products_path
+    end
+
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   
   resources :products
-  root 'home#index'
   
+  root 'home#index'
   get 'home/index'
 
-  get 'cart/index' => 'carts#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
-  resources :sessions, only: [:create, :destroy]
+  #Cart
+  get 'cart' => 'carts#index'
+  post 'finish' => 'carts#finish'
 
   #routes for google
   match 'auth/google_oauth2/callback', to: 'sessions#create', via: [:get, :post]
-
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  resources :sessions, only: [:create, :destroy]
 
 end
