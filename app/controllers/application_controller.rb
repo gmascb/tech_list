60bc3aa94e835720b1704a6bb0d3e57943f8a98c
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2021-03-23 19:24:33 -0300

    garantindo que o usuario tenha cart e q esteja setado o fav cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
     
     def validate_user
 
+        add_carts_to_user(current_user)
+
         controllers = ["home", "sessions"]
         msg = "É necessário estar logado para usar o sistema."
 
@@ -14,7 +16,14 @@ class ApplicationController < ActionController::Base
     end
 
     private 
-    
+
+    def add_carts_to_user(user)
+        if user && user.favorite_cart.nil?
+            cart = Cart.find_or_create_by!(user_id: user.id)
+            user.update!(favorite_cart: cart.id)
+        end
+    end
+ 
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
