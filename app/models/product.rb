496023a902dfdc53aa5a98e49237ca93c888e85a
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2021-03-23 19:37:31 -0300

    ajustes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+
+    validates :name, :category, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Criando produtos"
-Product.find_or_create_by!(name: "Café", shared: true, category: "Alimento")
-Product.find_or_create_by!(name: "Desodorante", shared: true, category: "Higiene Pessoal")
-Product.find_or_create_by!(name: "Granola", shared: true, category: "Alimento")
-Product.find_or_create_by!(name: "Shampoo", shared: true, category: "Higiene Pessoal")
 
-puts 'Criando usuario'
-User.find_or_create_by!(name: "Daniel")
-User.find_or_create_by!(name: "Jose")
+Product.find_or_create_by!(name: "Detergente", shared: true, category: "Limpeza")
+Product.find_or_create_by!(name: "Amaciante", shared: true, category: "Limpeza")
+Product.find_or_create_by!(name: "Sabão Lava Roupas", shared: true, category: "Limpeza")
+
+Product.find_or_create_by!(name: "Café", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Granola", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Carne Bovina", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Carne Suína", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Carne de Frango", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Maçã", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Banana", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Cerveja", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Leite", shared: true, category: "Alimento")
+Product.find_or_create_by!(name: "Yakult", shared: true, category: "Alimento")
+
+Product.find_or_create_by!(name: "Pasta de Dente", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Escova de Dente", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Papel Higiênico", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Desodorante", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Shampoo", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Sabonete", shared: true, category: "Higiene Pessoal")
+Product.find_or_create_by!(name: "Condicionador", shared: true, category: "Higiene Pessoal")
